1ca500bbaec2ee7838626d9714ec56e99b5bd1a4
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2022-06-24 18:34:20 +0530

    Created Mini Palette and styled it

---
+++ src/MiniPalette.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+    root: {
+        backgroundColor: 'white',
+        border: '1px solid black',
+        borderRadius: '5px',
+        padding: '0.5rem',
+        position: 'relative',
+        overflow: 'hidden',
+        '& hover': {
+            cursor: 'pointer'
+        }
+    },
+    colors: {
+        backgroundColor: '#dae1e4',
+        height: '100px',
+        width: '100%',
+        borderRadius: '5px',
+        overflow: 'hidden'
+    },
+    title: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: '0',
+        color: 'black',
+        paddingTop: '0.3rem',
+        paddingBottom: '0.5rem',
+        fontSize: '0.8rem',
+        position: 'relative'
+    },
+    emoji: {
+        marginLeft: '0.1rem',
+        fontSize: '1.0rem'
+    },
+    miniColor: {
+        height: '25%',
+        width: '20%',
+        display: 'inline-block',
+        margin: '0 auto',
+        position: 'relative',
+        marginBottom: '-4px'
+    }
+};
+
+function MiniPalette(props) {
+    const { classes, paletteName, emoji, colors } = props;
+    const miniColorBoxes = colors.map(color => (
+        <div className={classes.miniColor} key={color.name} style={{ background: color.color }}></div>
+    ))
+    return (
+        <div className={classes.root}>
+            <div className={classes.colors}>{miniColorBoxes}</div>
+            <h5 className={classes.title}>{paletteName}<span className={classes.emoji}>{emoji}</span></h5>
+        </div>
+    )
+}
+
+export default withStyles(styles)(MiniPalette);
